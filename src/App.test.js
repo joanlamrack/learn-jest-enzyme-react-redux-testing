41bde39491d793b0e82ce404e8bfc35190f975e1
joanlamrack <joanlamrack@gmail.com>
2018-09-07 09:45:26 +0700

    Add config Adapter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Adapter from "enzyme-adapter-react-16"
+
+configure({adapter: new Adapter()});
+
+it("renders without crashing", () => {
+	const div = document.createElement("div");
+	ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
+
+it("didnt have a state", () => {
+	const wrapper = shallow(<App />);
+	expect(wrapper.state).to.eql(undefined);
 });
