ce4fa939ac88d4cba63899e38e00c02068058b6e
joanlamrack <joanlamrack@gmail.com>
2018-09-07 09:49:38 +0700

    Add config

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { shallow } from "enzyme";
+import { shallow, configure } from "enzyme";
 
-import Adapter from "enzyme-adapter-react-16"
+import Adapter from "enzyme-adapter-react-16";
 
 configure({adapter: new Adapter()});
 
